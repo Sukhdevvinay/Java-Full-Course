b9519f74371a99967120c5cfd50f63f01fced0de
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-08-24 13:36:53 +0530

    Update Demo3.java

--- Lecture 49/code/Demo3.java
+++ Lecture 49/code/Demo3.java
@@ -17,6 +17,14 @@ public class Demo3 {
         t2.start();
 
     }
+    /*
+    Thread.yield();
+    Koi bhi thread jo current thread par jisme par hum Thread.yield laga rahe hain woh ye khaege 
+    ki agar main already run ho rahi hun aur uske alwa jitne bhi thread present hain , jinki priority
+    mere barabar hain aur unko agar cpu time nahi mil raha hain toh main khudka cpu time unko de dungi
+    and toh myslef running state se runnable main chale jaungi and weh runnable se running state main 
+    chale jayege
+    */
 }
 
 /*
@@ -24,10 +32,9 @@ public class Demo3 {
 Thread.yield() --> I am willing to give my cpu time to someone else with same priority and 
 that wants to run
 
+why we not use this in production -> 
 1. OS can reject this.
-
 2. It is like a suggestion to the OS
-
 3. Current thread does not go to WAITING, TIMED_WAITING, BLOCKED.
    It does go to only RUNNABLE state
 
