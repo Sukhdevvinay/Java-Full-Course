b94fdc49fd53a777f3d2a946ae4327c9092174ea
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-08-24 13:12:09 +0530

    Update Demo2.java

--- Lecture 49/code/Demo2.java
+++ Lecture 49/code/Demo2.java
@@ -12,9 +12,30 @@ public class Demo2 {
 
         t1.start();
 
-        //t1.join()
-        t1.join(1000); // let the t1 thread first complete its execution
+        //t1.join() // let the t1 thread first complete its execution
+        t1.join(1000);  // we can also give time to wait main thread for execution of t1 thread 
+        /* 
+        t1.join(); 
+        t1.join(x); Means ab hum apne main thread ko keh rahe hain ki sirf x seconds tak wait karana
+        , agar thread t1 x seconds main khatam ho rahi hain toh thik hain warna , agar x seconds se
+        jayada time laga rahi toh tum apna next line ke code ka execution start kar sakte hon
+        
+        Ye t1.join() method humne waha lagaya hain , jaha main thread chal rahi hain , toh hum 
+        main thread ko keh rahe hain ki , tum main thread waiting state par chale jao jab tak 
+        humare thread t1 khatam nahi ho jati means main thread waiting state par chale gayi jab tak 
+        thread t1 khatam nahi ho jati hain so ab t1.join() ke line ke baad jo code likha hain woh tab
+        tak nahi chalega or execute hoga jab tak thread t1 ka kaam khatam nahi ho jata hian.
+        */
 
+        /*
+        t1.join(); Ye Main thread ko waiting state main le jata hain , jab tak humare thread t1 khatam
+        nahi ho jati tab tak wait karta hian 
+
+        t1.join(x); Ye Main thread ko timed-waited state main le jata hian , means main thread only wait
+        for x seconds , agar woh x seconds ke andar khatam ho gayi toh sahi , warne phir main thread 
+        chale jayege.
+        */
+        
         System.out.println("Main thread ends");
     }
 }
