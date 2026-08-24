8f074ea3fd668a0da6cdfa927557a156711dff64
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-08-24 13:41:09 +0530

    Update Demo4.java

--- Lecture 49/code/Demo4.java
+++ Lecture 49/code/Demo4.java
@@ -29,6 +29,14 @@ public class Demo4 {
 
     -> sleep(), join(), wait() : TIMED_WAITING, WAITING --> interrupt()
 
-
+    A) Non-STATIC METHOD : 
+      Ye Hum kise thread par lagate hain , means this method kisi thread par lagate hian like
+      thread t1 , t2, t3,t4 , etcs;
+      ex : t1.start()
+      Means hum ye methods t1,t2,t3 .. tn par laga sakte hain 
+    B) Static Method : 
+    Ye Hum Thread object par lagate hian 
+    ex : 
+    Thread.yield
 
 */
