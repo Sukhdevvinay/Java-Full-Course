bb06b13d9463effd7126bf8444f9840e2dd27cb1
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-08-27 10:48:24 +0530

    Update Demo7.java

--- Lecture 49/code/Demo7.java
+++ Lecture 49/code/Demo7.java
@@ -20,11 +20,20 @@ public class Demo7 {
     Thread Priority
     MAX_PRIORITY = 10
     MIN_PRIORITY = 1
-    NORM_PRIORITY = 5
+    NORM_PRIORITY = 5 // by default priority for all threads
 
     Depends on OS 
     -> may respect Priority
     -> may partially repsect
     -> may not at all
+
+    Priority ka means Hum OS ko bta rahe hain ki yeh thread thoda jayada important hain toh esko 
+    thoda jayada run or execute karna means esko thodi jayada CPU cycle dena ya 
+    jiske priority jayada hogi hame usko run karna hoga.
+
+    But ese koi gurannte nahi hain jiske priority jayda hain woh phele run hoga , balki yeh 
+    uspe depend karta hain ki Humare OS pe load kitna hain ya baki thread ko kaise CPU cycle 
+    deta hain 
+    We can say that it depends on the OS for priority threads to run or not.
 
 */
