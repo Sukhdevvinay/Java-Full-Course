223c816a4fb3f624f5dc2c13de8615790aca94b7
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-09-06 17:38:05 +0530

    Document critical section and race condition concepts
    Added comments explaining critical sections and race conditions in multithreading.

--- Lecture 51/code/Demo.java
+++ Lecture 51/code/Demo.java
@@ -25,3 +25,26 @@ class Test {
         System.out.println(Thread.currentThread().getName() + "Show finish");
     }
 }
+
+/*
+Critical Section : Ek esa Section Jisme Hum chahe hain ki usme Ek Time par sirf Ek Hi thread 
+Enter kare. and Critical Section Main Hamare Pass Jo bhi Resource Ho ti hain usko Hum Shared 
+Resources Khete hain.
+Ex : 
+Class Counter {
+    int cnt = 0;
+    void increment() {
+        cnt++;
+    }
+};
+
+Ye Jo Increment Method Hain Woh ek Crtical Section Hain.
+and cnt++ is A shared Resource.
+So Problem is That : 
+Agar hum Multiple Threads ko iske Andar aane Do toh We have a problem of Race Condition.
+this is arise due to because cnt++ is non Atomic Operation.
+All non Atomic Opration Have Race Condition Problem.
+
+So Solution of This Problem is Keyword : Synchornized.
+
+*/
