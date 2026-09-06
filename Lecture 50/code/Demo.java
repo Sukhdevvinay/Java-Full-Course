0968a7df075db16bc010f4c93f4f1d27f9d5b39d
Sukhdevvinay <129602601+Sukhdevvinay@users.noreply.github.com>
2026-09-06 19:01:27 +0530

    Update Demo.java

--- Lecture 50/code/Demo.java
+++ Lecture 50/code/Demo.java
@@ -38,4 +38,36 @@ class Counter {
     }
 }
 
-// Critical Section
+/*
+    synchronized void increment() {
+            count++; // 3 operation
+    }
+
+    So AB Humara Critical Section is Increment Method toh hume is par Synchronized Keyword 
+    Lagana hain. 
+    Toh Ab Jab Humne Synchorized Keyword Lagya is Method Par .
+    * Toh Ab ye Critical Section Lock Ho gaya hain.
+*/
+
+/*
+Critical Section : Ek esa Section Jisme Hum chahe hain ki usme Ek Time par sirf Ek Hi thread 
+Enter kare. and Critical Section Main Hamare Pass Jo bhi Resource Ho ti hain usko Hum Shared 
+Resources Khete hain.
+Ex : 
+Class Counter {
+    int cnt = 0;
+    void increment() {
+        cnt++;
+    }
+};
+
+Ye Jo Increment Method Hain Woh ek Crtical Section Hain.
+and cnt++ is A shared Resource.
+So Problem is That : 
+Agar hum Multiple Threads ko iske Andar aane Do toh We have a problem of Race Condition.
+this is arise due to because cnt++ is non Atomic Operation.
+All non Atomic Opration Have Race Condition Problem.
+
+So Solution of This Problem is Keyword : Synchornized.
+
+*/
